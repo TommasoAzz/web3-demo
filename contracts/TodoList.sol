d13aeb5823389943e0f0f5b058805c27fbdca6aa
Tommaso Azzalin <azzalintommaso@gmail.com>
2021-11-11 17:59:08 +0100

    Added partial contract

---
+++ contracts/TodoList.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.16 <0.9.0;
+
+// The To-Do item itself.
+struct Todo {
+    // What action should be performed.
+    string text;
+
+    // Flag controlling whether the item has been completed.
+    bool done;
+}
+
+contract TodoList {
+    
+}
